ec032a433e4c4b45842872970a2be7785cfd9cb4
Eric Lange <eric@flicket.tv>
2017-03-13 17:10:15 +0530

    Added HelloConsole example

---
+++ HelloConsole/Server/console.js
@@ -0,0 +1,10 @@
+// This will keep the process alive until an explicit process.exit() call
+setInterval(function(){},1000)
+
+// This will attempt to attach to a ConsoleSurface
+LiquidCore.attach('org.liquidplayer.surfaces.console.ConsoleSurface', (error) => {
+    console.log("Hello, Console!")
+    if (error) {
+        console.error(error)
+    }
+})
